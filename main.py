19ba2cabd73a71d908e68b90d420236642aa81e3
Knightz____ <knightz0000@protonmail.com>
2024-08-26 10:32:49 -0300

    feature: Add Asteroids and asteroids field to the program, add player class with rotation and movement, add shapes to asteroids and player

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    asteroid_field = AsteroidField()
+
     while True:
-        pygame.Surface.fill(screen, color="black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for values in updatable:
+            values.update(dt)
+        screen.fill("black")
+        for values in drawable:
+            values.draw(screen)
+        pygame.display.flip()
+      
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
